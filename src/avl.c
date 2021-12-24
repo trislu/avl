3e538dd47c8d93d48f06151495bc868981641e95
ltristank <lukai@mail.ustc.edu.cn>
2021-12-24 15:49:15 +0800

    bug fix : wrong condition

--- src/avl.c
+++ src/avl.c
@@ -363,7 +363,7 @@ static avl_set_element *__avl_set_insert(struct avl_set *s, avl_set_element *e, 
     {
         /*! @note on edge */
         size_t empty_slot = 0;
-        if (0 == __avl_stack_pop(&empty_slot, s->_slots))
+        if (0 != __avl_stack_pop(&empty_slot, s->_slots))
         {
             assert(0);
             return NULL;
@@ -906,7 +906,7 @@ static avl_map_element *__avl_map_insert(struct avl_map *s, avl_map_element *e, 
     {
         /*! @note on edge */
         size_t empty_slot = 0;
-        if (0 == __avl_stack_pop(&empty_slot, s->_slots))
+        if (0 != __avl_stack_pop(&empty_slot, s->_slots))
         {
             assert(0);
             return NULL;
